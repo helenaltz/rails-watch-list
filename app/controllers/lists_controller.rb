45e5b667abd69b0110acea68df0e7dd463f51e71
helene letouze <helene.letouze@yahoo.fr>
2024-03-07 17:19:52 +0100

    exo2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,26 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    id = params[:id]
+    @list = List.find(id)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    @list.save!
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+end
